b57e1a06c00a97ad7dda0e4106cb25189a13eb1d
Nulerein <davidza020306@gmail.com>
2025-03-24 22:21:57 +0300

    )

--- c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0f2eb7063f5a0938cd11632f67e932b08b7c83")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
