f45b60af574781315e5081f4f4d368f07689306f
Nulerein <davidza020306@gmail.com>
2026-01-31 00:49:15 +0300

    Контрольная точка из VS Code для сеанса облачного агента

--- c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
+++ c#/obj/Debug/net8.0/c#.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c#")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe0f2eb7063f5a0938cd11632f67e932b08b7c83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b204123caf969691af1a48252b7cea3b1e541e74")]
 [assembly: System.Reflection.AssemblyProductAttribute("c#")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c#")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
